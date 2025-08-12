e29e9ec5e8aa39ec6377dedec0fcc8270ba17a4e
priyanshsingh <singh.priyansh1391@gmail.com>
2025-08-12 15:48:11 +0530

    db

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,8 +4,6 @@ const connectDB = async() => {
     try {
         mongoose.connection.on('connected', ()=> console.log('Database connected') )
         await mongoose.connect(`${process.env.MONGODB_URL}/pingup`,{
-             useNewUrlParser: true,
-    useUnifiedTopology: true
     })
     } catch (error) {
         console.log(error.message)
